c91cf05fb0f6acac45a0b7a67c1ff29f49c2281c
Jan Křížek <jkriz@K911-DOMA>
2026-02-10 18:16:44 +0100

    test

--- CommWindow/Configurations/ConfigMainWindow.cs
+++ CommWindow/Configurations/ConfigMainWindow.cs
@@ -5,20 +5,11 @@ namespace CommWindow.Configurations
 {
     public class ConfigMainWindow : IConfigMainWindow
     {
-        private ServiceProvider? _serviceProvider;
-        private readonly AppConfigurations _appConfig;
-        private readonly ServiceCollection _serviceColl;        
-
-        public ConfigMainWindow(ServiceProvider serviceProvider, AppConfigurations appConfig, ServiceCollection serviceColl)
-        {
-            _serviceProvider = serviceProvider;
-            _serviceColl = serviceColl;
-            _appConfig = appConfig;
-        }
-
+        private ServiceProvider? _serviceProvider;                
+               
         public void ConfigWindow()
-        {
-            _serviceColl.AddSingleton(_appConfig.ConfigurationBuilderSetup());
+        {            
+            var _serviceColl = new ServiceCollection();
             _serviceColl.AddSingleton<MainWindow>();
             _serviceProvider = _serviceColl.BuildServiceProvider();
             var mainWindow = _serviceProvider.GetRequiredService<MainWindow>();
